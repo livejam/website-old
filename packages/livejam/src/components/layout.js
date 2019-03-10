9e0a35f9706dd3523af33917d6b836206665c490
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2019-03-10 23:30:27 +0100

    Add featured livejam

--- packages/livejam/src/components/layout.js
+++ packages/livejam/src/components/layout.js
@@ -2,16 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActions from "@material-ui/core/CardActions";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
 import { withStyles } from "@material-ui/core/styles";
 
 import TopBar from "./top-bar";
-import SpeakerCard from "./speaker-card";
+import { Divider } from "@material-ui/core";
 
 const Layout = ({ children, classes }) => (
   <StaticQuery
@@ -36,56 +42,82 @@ const Layout = ({ children, classes }) => (
             }
           }
         }
+        livejam: file(relativePath: { eq: "livejam.png" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     `}
-    render={({ site, sebastiankorfmann, mikebild }) => {
+    render={({ site, livejam, sebastiankorfmann, mikebild }) => {
       return (
         <div className={classes.root}>
           <CssBaseline />
           <TopBar title={site.siteMetadata.title} />
           <main className={classes.main}>{children}</main>
 
-          <Divider />
           <div className={classes.layout}>
-            <Typography variant="h2" color="inherit">
-              Yeah
-            </Typography>
-
-            <Grid container spacing={2}>
+            <Grid
+              container
+              justify="center"
+              spacing={2}
+              className={classes.featuredContainer}
+            >
               <Grid item xs={12} md={8}>
                 <Card>
-                  <CardHeader>foo</CardHeader>
+                  <CardHeader
+                    title="Building Livejam.io - 5th Iteration"
+                    subheader="March 14, 2019"
+                    action={
+                      <IconButton>
+                        <MoreVertIcon />
+                      </IconButton>
+                    }
+                  />
+
+                  <CardMedia
+                    className={classes.media}
+                    image={livejam.childImageSharp.fluid.src}
+                    title="Paella dish"
+                  />
                   <CardContent>
-                    bar
-                    <SpeakerCard
-                      image={sebastiankorfmann.childImageSharp.fluid.src}
-                      headline="Sebastian Korfmann"
-                      link="https://www.skorfmann.com"
-                    />
-                    <SpeakerCard
-                      image={mikebild.childImageSharp.fluid.src}
-                      headline="Mike Bild"
-                      link="https://www.mikebild.com"
-                    />
+                    <Grid container justify="center" alignItems="center">
+                      <Grid item xs={6} md={3}>
+                        <Avatar
+                          alt="Mike Bild"
+                          src={mikebild.childImageSharp.fluid.src}
+                          className={classes.avatar}
+                        />
+                      </Grid>
+                      <Grid item xs={6} md={3}>
+                        <Avatar
+                          alt="Sebastian Korfmann"
+                          src={sebastiankorfmann.childImageSharp.fluid.src}
+                          className={classes.avatar}
+                        />
+                      </Grid>
+
+                      <Grid item xs={12} md={5}>
+                        <Typography
+                          variant="subtitle1"
+                          className={classes.connectorText}
+                        >
+                          Join Mike and Sebastian's live jam session building
+                          Livejam.io.
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Card>
-                  <CardHeader>foo</CardHeader>
-                  <CardContent>
-                    bar
-                    <SpeakerCard
-                      image={sebastiankorfmann.childImageSharp.fluid.src}
-                      headline="Sebastian Korfmann"
-                      link="https://www.skorfmann.com"
-                    />
-                    <SpeakerCard
-                      image={mikebild.childImageSharp.fluid.src}
-                      headline="Mike Bild"
-                      link="https://www.mikebild.com"
-                    />
-                  </CardContent>
+                  <CardActions className={classes.actions}>
+                    <IconButton aria-label="Add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="Share">
+                      <ShareIcon />
+                    </IconButton>
+                  </CardActions>
                 </Card>
               </Grid>
             </Grid>
@@ -106,11 +138,36 @@ const styles = theme => ({
     minHeight: "100%",
     position: "relative"
   },
+  featured: {
+    marginTop: theme.spacing(5)
+  },
+  actions: {
+    display: "flex"
+  },
+  featuredContainer: {
+    marginTop: theme.spacing(7)
+  },
+
+  avatar: {
+    margin: 10,
+    width: 150,
+    height: 150
+  },
+
+  connectorText: {
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2)
+  },
+
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
   layout: {
     width: "auto",
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1400 + theme.spacing.unit * 3 * 2)]: {
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(1400 + theme.spacing(6))]: {
       width: 1400,
       marginLeft: "auto",
       marginRight: "auto"
@@ -119,7 +176,7 @@ const styles = theme => ({
   main: {},
   footer: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing.unit * 6,
+    padding: theme.spacing(6),
     position: "static",
     bottom: 0,
     width: "100%"
